ce904459e0cec1699b35fa5ef361e51a8b9e8672
Kanishk Gandhi <kvg245@cassio.cs.nyu.edu>
2018-11-19 20:54:46 -0500

    add pretrain

---
+++ models.py
@@ -0,0 +1,244 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+base_conv_cfg = {
+    'num_layers':2,
+    'inp': [3,16],
+    'out': [16,32],
+    'stride': [4,2],
+    'kernel': [8,4],
+    'pool': [False,False]}
+
+stack_conv_cfg = {
+    'num_layers':2,
+    'inp': [6,32],
+    'out': [32, 64],
+    'stride': [4,2],
+    'kernel': [8,4],
+    'pool': [False,False]}
+
+
+
+class DeConvBlock(nn.Module):
+
+  def __init__(self, cfg):
+    super(DeConvBlock, self).__init__()
+
+    self.layers = nn.ModuleList([])
+
+    for l in reversed(range(cfg['num_layers'])):
+      self.layers += [nn.ConvTranspose2d(
+        cfg['out'][l],
+        cfg['inp'][l],
+        cfg['kernel'][l],
+        cfg['stride'][l]
+        ), nn.ReLU()]
+
+  def forward(self,x):
+    for layer in self.layers:
+       x = layer(x)
+    return x
+
+class ConvBlock(nn.Module):
+
+  def __init__(self, cfg):
+    super(ConvBlock, self).__init__()
+
+    self.layers = nn.ModuleList([])
+    for l in range(cfg['num_layers']):
+      self.layers += [nn.Conv2d(
+        cfg['inp'][l],
+        cfg['out'][l],
+        cfg['kernel'][l],
+        cfg['stride'][l]
+        ), nn.ReLU()]
+
+  def forward(self,x):
+    for layer in self.layers:
+       x = layer(x)
+    return x
+
+class MiniVAE(nn.Module):
+  def __init__(self, hidden_size =256):
+    super(MiniVAE, self).__init__()
+
+    self.hidden_size = hidden_size
+    self.conv_block = ConvBlock(base_conv_cfg)
+    self.fc_enc = nn.Linear(2592, hidden_size*2)
+    self.fc_enc_m = nn.Linear(hidden_size*2, hidden_size)
+    self.fc_enc_s = nn.Linear(hidden_size*2, hidden_size)
+    
+    self.fc_dec = nn.Linear(hidden_size, hidden_size*2)
+    self.fc_dec2 = nn.Linear(hidden_size*2, 2592)
+    self.deconv_block = DeConvBlock(base_conv_cfg)
+
+  def reparameterize(self, mu, logvar):
+    std = torch.exp(0.5*logvar)
+    eps = torch.randn_like(std)
+    return eps.mul(std).add_(mu)
+
+  def encode(self, x):
+    x = self.conv_block(x)
+    x = x.view(-1, 2592)
+    x = F.relu(self.fc_enc(x))
+    mu = F.relu(self.fc_enc_m(x))
+    logvar = F.relu(self.fc_enc_s(x))
+    return mu, logvar
+    
+  def decode(self, z):
+    y = F.relu(self.fc_dec(z))
+    y = F.relu(self.fc_dec2(y))
+    y = y.view(-1, 32, 9, 9)
+    y = self.deconv_block(y)
+    return y
+
+  def forward(self, x):
+    mu, logvar = self.encode(x)
+    z = self.reparameterize(mu, logvar)
+    y = self.decode(z)
+    return y, mu, logvar
+
+class SimpleLSTM(nn.Module):
+
+  def __init__(self, hidden_size=256, cell_type='lstm'):
+    super(SimpleLSTM, self).__init__()
+
+    self.conv_block = ConvBlock(base_conv_cfg)
+    self.hidden_size = hidden_size
+    self.fc1 = nn.Linear(2592,hidden_size)
+    if cell_type=='lstm':
+      self.rnn = nn.LSTM(hidden_size, hidden_size)
+    elif cell_type=='gru':
+      self.rnn = nn.GRU(hidden_size, hidden_size)
+    self.fc2 = nn.Linear(hidden_size, 2)
+
+  def forward(self, x, y):
+    x = self.conv_block(x)
+    y = self.conv_block(y)
+    x = x.view(-1,2592).unsqueeze(0)
+    y = y.view(-1,2592).unsqueeze(0)
+    x = F.relu(self.fc1(x))
+    y = F.relu(self.fc1(y))
+    seq = torch.cat([x,y],0)
+    out, _ = self.rnn(seq)
+    out = self.fc(out[-1,:,:])
+    return out
+
+class Siamese(nn.Module):
+
+  def __init__(self, hidden_size=256):
+    super(Siamese, self).__init__()
+
+    self.conv_block = ConvBlock(base_conv_cfg)
+    self.hidden_size = hidden_size
+    self.fc1 = nn.Linear(5184, hidden_size)
+    self.fc2 = nn.Linear(hidden_size, 2)
+
+  def forward(self, x, y):
+    x = self.conv_block(x)
+    y = self.conv_block(y)
+    x = x.view(-1,2592)
+    y = y.view(-1,2592)
+    out = torch.cat([x,y],1)
+    out = F.relu(self.fc1(out))
+    out = self.fc2(out)
+    return out
+
+class Diffnet(nn.Module):
+
+  def __init__(self, hidden_size=256):
+    super(Diffnet, self).__init__()
+
+    self.conv_block = ConvBlock(base_conv_cfg)
+    self.hidden_size = hidden_size
+    self.fc1 = nn.Linear(2592, hidden_size)
+    self.fc2 = nn.Linear(hidden_size, 2)
+
+  def forward(self, x, y):
+    out = self.conv_block(x-y)
+    out = out.view(-1,2592)
+    out = F.relu(self.fc1(out))
+    out = self.fc2(out)
+    return out
+
+class SiameseDiffnet(nn.Module):
+
+  def __init__(self, hidden_size=256, mode='dif'):
+    super(SiameseDiffnet, self).__init__()
+
+    self.conv_block = ConvBlock(base_conv_cfg)
+    self.hidden_size = hidden_size
+    self.fc1 = nn.Linear(2592, hidden_size)
+    self.fc2 = nn.Linear(hidden_size, 2)
+    self.mode = mode
+
+  def forward(self, x, y):
+    x = self.conv_block(x)
+    y = self.conv_block(y)
+    x = x.view(-1,2592)
+    y = y.view(-1,2592)
+    if self.mode == 'dif':
+      out = x-y
+    elif self.mode == 'dot':
+      out = torch.mul(x, y)
+    out = F.relu(self.fc1(out))
+    out = self.fc2(out)
+    return out
+
+class Stacknet(nn.Module):
+
+  def __init__(self, hidden_size=256, mode='dif'):
+    super(Stacknet, self).__init__()
+
+    self.conv_block = ConvBlock(stack_conv_cfg)
+    self.hidden_size = hidden_size
+    self.fc1 = nn.Linear(5184, hidden_size)
+    self.fc2 = nn.Linear(hidden_size, 2)
+    self.mode = mode
+
+  def forward(self, x, y):
+    out = torch.cat([x,y],1)
+    out = self.conv_block(out)
+    out = out.view(-1,5184)
+    out = F.relu(self.fc1(out))
+    out = self.fc2(out)
+    return out
+
+class SiameseOpnet(nn.Module):
+
+  def __init__(self, hidden_size=256, mode='dif'):
+    super(SiameseOpnet, self).__init__()
+
+    self.conv_block = ConvBlock(base_conv_cfg)
+    self.hidden_size = hidden_size
+    self.fc1 = nn.Linear(2592, hidden_size)
+    # self.c1 = nn.Conv2d(2,1,1)
+    self.w = nn.Parameter(torch.randn(1))
+    self.v = nn.Parameter(torch.randn(1))
+    # self.w = 1
+    # self.v = 1
+
+
+
+
+    self.fc2 = nn.Linear(hidden_size, 2)
+
+  def forward(self, x, y):
+    x = self.conv_block(x)
+    y = self.conv_block(y)
+    x = x.view(-1,2592)
+    y = y.view(-1,2592)
+
+    out = self.w*x-self.v*y
+    # out = torch.stack([x,y],dim=1).unsqueeze(3)
+    # out = self.c1(out)
+    # out = out.view(-1,self.hidden_size)
+
+    out = F.relu(self.fc1(out))
+    # y = F.relu(self.fc1(y))
+    out = self.fc2(out)
+    return out
+
+
--- pre_train.py
+++ pre_train.py
@@ -1,16 +1,16 @@
 import torch
 import torch.nn as nn
 from torchvision import transforms, datasets
+from torch.nn import functional as F
 
-from data import ChangeDetectionDataset
-
+from data import PreChangeDetectionDataset
 from models import *
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print(device)
-PATH = '/data/kvg245/change_test/checkpoints'
-change_detection_dataset = ChangeDetectionDataset(data_dir='/data/kvg245/',
+PATH = '/data/kvg245/change_test/checkpoints/pretrain_vae.ckpt'
+change_detection_dataset = PreChangeDetectionDataset(data_dir='/data/kvg245/',
                                                   transforms = transforms.Compose([
                                                     transforms.ToTensor()
                                                     ]))
@@ -30,17 +30,23 @@ val_loader = torch.utils.data.DataLoader(val_dataset,
 test_loader = torch.utils.data.DataLoader(test_dataset,
                                                  batch_size=batch_size, shuffle=False, num_workers=4)
 
-model = SiameseOpnet()
+model = MiniVAE().to(device)
+
 print model
 
-criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+def kl_loss(mu, logvar):
+  KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+  return KLD
+
+criterion = nn.MSELoss()
 for epoch in range(num_epochs):
-  for i, sample in enumerate(train_loader):
+  for i, image in enumerate(train_loader):
     optimizer.zero_grad()
-    outputs = model(sample['a'], sample['b'])
-    loss = criterion(outputs, sample['c'])
+    image = image.cuda()
+    y, mu, logvar = model(image)
+    loss = 50* criterion(y, image)+kl_loss(mu, logvar)
     loss.backward()
     optimizer.step()
 
@@ -48,30 +54,7 @@ for epoch in range(num_epochs):
       # print outputs, sample['c']
       print ('Epoch: [%d/%d], Step: [%d/%d], Loss: %.4f' 
            % (epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
-
-  correct = 0
-  total = 0
-
-  for sample in val_loader:
-    outputs = model(sample['a'], sample['b'])
-    _, predicted = torch.max(outputs.data, 1)
-    total += sample['c'].size(0)
-    correct += (predicted == sample['c']).sum()
-  print('Accuracy of the model on the val images: %d %%' % (100 * correct / total))
-  if (epoch+1)%5 == 0:
+  if (epoch+1)%2 == 0:
     torch.save(model, PATH)
-correct = 0
-total = 0
-
-for sample in test_loader:
-  outputs = model(sample['a'], sample['b'])
-  _, predicted = torch.max(outputs.data, 1)
-  total += sample['c'].size(0)
-  correct += (predicted == sample['c']).sum()
-print('Accuracy of the model on the test images: %d %%' % (100 * correct / total))
-
-
-
-
 
 
